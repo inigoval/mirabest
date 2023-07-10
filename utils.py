cce51537196aa56a47c05ff376ab0be243f29a9f
Inigo Val Slijepcevic <inigoval@gmail.com>
2023-07-10 13:56:27 +0100

    Add config file

--- utils.py
+++ utils.py
@@ -1,7 +1,19 @@
 from pathlib import Path
+import yaml
 
 
 def create_path(*args):
     for path in args:
         if not Path.exists(path):
             Path.mkdir(path, parents=True)
+
+
+def load_config():
+    """Helper function to load yaml config file, convert to python dictionary and return."""
+
+    # load global config
+    global_path = "config.yml"
+    with open(global_path, "r") as ymlconfig:
+        config = yaml.load(ymlconfig, Loader=yaml.FullLoader)
+
+    return config
